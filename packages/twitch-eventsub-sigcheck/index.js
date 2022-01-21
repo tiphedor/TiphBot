5af9db5bb1cfb09fdf15156d4c8cea5d0482638c
Martin STEFFEN <mail@tiph.io>
2022-01-21 12:58:11 +0100

    utils functions, handle challenges and other events, tweaks

--- packages/twitch-eventsub-sigcheck/index.js
+++ packages/twitch-eventsub-sigcheck/index.js
@@ -11,6 +11,13 @@ const MESSAGE_TYPE_VERIFICATION = 'webhook_callback_verification';
 const MESSAGE_TYPE_NOTIFICATION = 'notification';
 const MESSAGE_TYPE_REVOCATION = 'revocation';
 
+const isChallenge = (messageType) => messageType === MESSAGE_TYPE_VERIFICATION;
+const isNotification = (messageType) =>
+  messageType === MESSAGE_TYPE_NOTIFICATION;
+const isRevocation = (messageType) => messageType === MESSAGE_TYPE_REVOCATION;
+
+const handleChallenge = (body) => body.challenge;
+
 /**
  * Looks for the header nammes `headerName` in the object `headers`. If `headerName` can't be find,
  * this function will check for the lower-case version of `headerName`.
@@ -30,7 +37,7 @@ const getHeader = (headers, headerName) => {
  * @param body {string} body of the incoming request
  * @param secret {string} secret to use for the signature check - the default value is the test secret from Twitch - not an actual secret ;)
  * @param verbose {boolean} Whether this function should log a log of things. Default to false.
- * @returns {boolean} Whether the incoming message matches the signature.
+ * @returns {object} If the verification failed, returns { verificationStatus: false }, otherwise returns { verificationStatus: true, parsedBody: object, messageType: string }
  */
 const signatureCheck = (
   headers,
@@ -49,18 +56,30 @@ const signatureCheck = (
     !incomingMessageSignature ||
     !incomingMessageType
   ) {
-    verbose &&
-      console.log(
-        `Missing at least one required header ! ${headers.toString()}`
-      );
+    console.error(
+      '@tiphbot/twitch-eventsub-sigcheck',
+      `Missing at least one required header ! ${headers.toString()}`
+    );
 
-    return false;
+    return { verificationStatus: false };
   }
 
+  // Body
   if (!body) {
-    verbose && console.log(`Body must be defined.`);
+    console.error('@tiphbot/twitch-eventsub-sigcheck', 'Body must be defined.');
 
-    return false;
+    return { verificationStatus: false };
+  }
+
+  let parsedBody = null;
+  try {
+    parsedBody = JSON.stringify(body);
+  } catch (e) {
+    console.warn(
+      '@tiphbot/twitch-eventsub-sigcheck',
+      "Warn: received body was not JSON-serializable. That's weird. We go:",
+      body.toString()
+    );
   }
 
   // Compute HMAC signature
@@ -78,44 +97,28 @@ const signatureCheck = (
       Buffer.from(incomingMessageSignature)
     )
   ) {
-    verbose &&
-      console.log(`@tiphbot/twitch-eventsub-sigcheck: Sigcheck failed !`);
+    console.error('@tiphbot/twitch-eventsub-sigcheck', 'Sigcheck failed !');
+    console.error('Expected: ', Buffer.from(computedHmacSignature));
+    console.error('Got:      ', Buffer.from(incomingMessageSignature));
 
-    return false;
+    return { verificationStatus: false };
   }
 
-  verbose && console.log(`@tiphbot/twitch-eventsub-sigcheck: Sigcheck OK!`);
-
-  if (MESSAGE_TYPE_NOTIFICATION === incomingMessageType) {
-    return true;
-  }
-
-  if (MESSAGE_TYPE_VERIFICATION === incomingMessageType) {
-    // @TODO: Handle Challenge?
-    verbose &&
-      console.log(
-        '@tiphbot/twitch-eventsub-sigcheck: Unsupported (yet) event type `challenge`, returning true since signature matched tho.'
-      );
-
-    return true;
-  }
-
-  if (MESSAGE_TYPE_REVOCATION === incomingMessageType) {
-    // @TODO: Handle Revocation?
-    verbose &&
-      console.log(
-        '@tiphbot/twitch-eventsub-sigcheck: Unsupported (yet) event type `revocation`, returning true since signature matched tho.'
-      );
-
-    return true;
-  }
-
-  // @TODO: Handle unknown type?
-  verbose &&
-    console.log(
-      `@tiphbot/twitch-eventsub-sigcheck: Unknown event type \`${incomingMessageType}\`, returning true since signature matched tho.`
-    );
-  return true;
+  verbose && console.log('@tiphbot/twitch-eventsub-sigcheck', 'Sigcheck OK!');
+  return {
+    verificationStatus: true,
+    messageType: incomingMessageType,
+    parsedBody,
+  };
 };
 
-module.exports = { signatureCheck };
+module.exports = {
+  signatureCheck,
+  MESSAGE_TYPE_VERIFICATION,
+  MESSAGE_TYPE_NOTIFICATION,
+  MESSAGE_TYPE_REVOCATION,
+  isChallenge,
+  isNotification,
+  isRevocation,
+  handleChallenge,
+};
